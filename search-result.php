19dd86895026e0a2c6cee61000870f3148f1bdb5
deepakpathania <deepakpathania789@gmail.com>
2016-12-06 23:26:30 +0530

    search result modified

--- search-result.php
+++ search-result.php
@@ -12,7 +12,7 @@
                             <div id="content">
                               <div class="row">
                                   <div class="col-md-12">
-                                      <div class="panel panel-default" style="margin: 20px 0px;">
+                                      <div style="margin: 20px 0px;">
                                           <div class="panel-heading">                                 
 <?php 
     if($number > 1){ 
@@ -41,25 +41,27 @@
         if( mysqli_num_rows($sql) > 0) {
             while($rws = mysqli_fetch_array($sql)){
 ?>
-                                                                    <div class="panel-group" id="panel-<?php echo $rws['user_id']; ?>">
+                                                                    <div class="panel-group dshadow" id="panel-<?php echo $rws['user_id']; ?>">
                                                                         <div class="panel panel-default">
                                                                             <div class="panel-heading">
                                                                                  <a class="panel-title" data-toggle="collapse" data-parent="#panel-<?php echo $rws['user_id']; ?>" href="#panel-element-<?php echo $rws['user_id']; ?>"><?php echo $rws['user_firstname'];?> <?php echo $rws['user_lastname'];?><button type="button" class="close" data-dismiss="panel"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></a>
                                                                             </div>
                                                                             <div id="panel-element-<?php echo $rws['user_id']; ?>" class="panel-collapse collapse in">
                                                                                 <div class="panel-body">
-                                                                                    <div class="col-md-6 column">
+                                                                                    <div class="col-md-4 column">
                                                                                         <img src="userfiles/avatars/<?php echo $rws['user_avatar'];?>" name="aboutme" class="img-responsive">                                  
                                                                                     </div>
-                                                                                    <div class="col-md-6 column">
+                                                                                    <div class="col-md-8 column">
                                                                                         <div class="col-md-12 column">
-                                                                                            <h2><span><?php echo $rws['user_firstname'];?> <?php echo $rws['user_lastname'];?></span></h2>
+                                                                                            <h2 class="black bold"><span><?php echo $rws['user_firstname'];?> <?php echo $rws['user_lastname'];?></span></h2>
                                                                                             <hr>
-                                                                                            <p><?php echo $rws['user_shortbio'];?></p>
+                                                                                            <p><b>Email id : </b><?php echo $rws['user_email'];?></p>
+                                                                                            <p><b>Gender : </b><?php echo $rws['user_gender'];?></p>
+                                                                                            <p><b>Short Bio : </b><?php echo $rws['user_shortbio'];?></p>
                                                                                             <hr>
                                                                                         </div>
                                                                                         <div class="col-md-4 column">
-                                                                                             <a href="profile.php?user_username=<?php echo $rws['user_username'];?>"><button type="button" class="btn btn-primary">View</button></a>
+                                                                                             <a href="profile.php?user_username=<?php echo $rws['user_username'];?>"><button type="button" class="btn btn-primary zbor mleft">View</button></a>
                                                                                         </div>
                                                                                     </div>
                                                                                 </div>
